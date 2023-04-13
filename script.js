38803a3b15c258a6572cef270761d0bd82a2a3c0
Ducphan2k <119691110+Ducphan2k@users.noreply.github.com>
2023-04-13 21:18:49 +0700

    tinhchuvi-dientich-hinhtron

---
+++ script.js
@@ -0,0 +1,15 @@
+// chu vi hinh tron
+
+// const PI = 3.14;
+// let r = prompt('nhap ban kinh hinh tron');
+// let c = 2 * PI * r;
+
+// document.write(`chu vi cua hinh tron la : ${c}m`)
+
+// dien tich hinh tron
+
+const PI = 3.14;
+let r = prompt("nhap ban kinh hinh tron");
+let s = PI * (r * r);
+
+document.write(`dien tich cua hinh tron la ${s}m`);
